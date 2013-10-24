d8fb7d2ddeb969ad32a88ed300ceabe09456efdf
Luca de Alfaro <luca@dealfaro.org>
2013-10-23 17:50:13 -0700

    Just added author info.

--- controllers/default.py
+++ controllers/default.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
 
-#########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - call exposes all registered services (none by default)
-#########################################################################
-
+# Author: Luca de Alfaro
 
 def index():
     """
